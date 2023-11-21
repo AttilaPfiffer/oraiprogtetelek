9003f9016ea884fdd30d854e7b4a3af85def2dd9
AttilaPfiffer <pifuati@gmail.com>
2023-11-21 18:05:53 +0100

    hazi feladat

--- feladatok.py
+++ feladatok.py
@@ -36,3 +36,46 @@ def feladat3(szoveg:str):
         i -= 1
 
 
+def hazi1():
+    print("1. feladat")
+    hettel_oszthato: int = 0
+    for i in range(1, 1001):
+        if i % 7 == 0:
+            hettel_oszthato += 1
+    return hettel_oszthato
+
+def hazi2():
+    print("2. feladat")
+    tizenkettovel_oszthato: int = 0
+    for i in range(2000, 20001):
+        if i % 12 == 0:
+            tizenkettovel_oszthato += 1
+    return tizenkettovel_oszthato
+
+def hazi3():
+    print("3. feladat")
+    osszeg: int = 0
+    szamok: int = 0
+    szam: int = 0
+    while szamok < 20:
+        if szam % 3 == 0:
+            osszeg += szam ** 2
+            szamok += 1
+        szam += 1
+    return osszeg
+
+def hazi4(szam:int):
+    print("4. feladat")
+    szam_osztok:int = 0
+    for i in range(1, szam + 1):
+        if szam % i == 0:
+            szam_osztok += 1
+    return szam_osztok
+
+def hazi5(szam:int):
+    print("5. feladat")
+    i: int = 0
+    while i > 1:
+        if szam % i == 0:
+            i -= 1
+    return szam
